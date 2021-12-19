7e6ca6d01628559a2e802f8c38326e6b106c4d7c
Alexander <electrodeyt@gmail.com>
2021-12-19 18:40:10 +0100

    Scripts: fix run_test.sh It now handles service compile errors

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,9 @@ make clean
 make compile-services
 if ! (test -f "sysroot/testa.elf" && test -f "sysroot/testb.elf";); then
   echo "Test services failed to compile"; false
+else
+  trap '' INT; make qemu-debug -j16; true
 fi
-trap '' INT; make qemu-debug -j16; true
+# trap '' INT; make qemu-debug -j16; true
 make clean
 make cloc
