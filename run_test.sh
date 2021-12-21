e4e99936eb3046742e2808dffe4f28ab785f01d8
Alexander <electrodeyt@gmail.com>
2021-12-21 19:28:36 +0100

    Scripts: run check-path.sh in compile/run_test

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 # wow such complicated
+if ! ( ./check-path.sh ); then
+  echo check-path.sh failed, source change-path.sh
+  exit -1
+fi
 make clean
 make compile-services
 if ! (test -f "sysroot/testa.elf" && test -f "sysroot/testb.elf";); then
